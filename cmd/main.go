3a12d60becf89bbb0ef65102caa2530875b10b46
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-21 22:09:26 +0900

    8-2. コマンドラインツール

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 
 	uas "nrsDDD/application/user"
 	us "nrsDDD/domain/services/user"
@@ -22,61 +24,57 @@ func main() {
 		log.Fatal(err)
 	}
 
-	userApplicationService, err := uas.New(userRepository, *userService)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	userRegisterService, err := uas.NewUserRegisterService(userRepository, *userService)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	userRegisterCommand, err := uas.NewUserRegisterCommand("Kenta")
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		fmt.Println("Input user name")
+		fmt.Print("> ")
 
-	err = userRegisterService.Handle(*userRegisterCommand)
-	if err != nil {
-		log.Fatal(err)
-	}
+		sc := bufio.NewScanner(os.Stdin)
 
-	user, err := userApplicationService.Get("8752be00-0611-4846-b02a-87dc5b330d98")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if user == nil {
-		log.Fatal("ユーザーが存在しません")
-	}
-	fmt.Println(*user)
+		var name string
+		if sc.Scan() {
+			name = sc.Text()
+		}
 
-	updateCommand, err := uas.NewUserUpdateCommand(user.Id)
-	if err != nil {
-		log.Fatal(err)
+		userRegisterCommand, err := uas.NewUserRegisterCommand(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = userRegisterService.Handle(*userRegisterCommand)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("--------")
+		fmt.Printf("user %v created\n", name)
+		fmt.Println("--------")
+
+		var flg bool
+		for {
+			fmt.Println("continue? (y/N)")
+			fmt.Print("> ")
+
+			var yesOrNo string
+			if sc.Scan() {
+				yesOrNo = sc.Text()
+			}
+
+			if yesOrNo == "y" {
+				break
+			} else if yesOrNo == "N" {
+				flg = true
+				break
+			}
+
+			fmt.Println("invalid command")
+		}
+		if flg {
+			break
+		}
 	}
-
-	err = updateCommand.SetName("Ken")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	user, err = userApplicationService.Update(*updateCommand)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	userDeleteService, err := uas.NewUserDeleteService(userRepository)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	deleteCommand, err := uas.NewUserDeleteCommand(user.Id)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = userDeleteService.Handle(*deleteCommand)
-
-	fmt.Println(*user)
 }
