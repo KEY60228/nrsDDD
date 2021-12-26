4a5179d8127f5f36111c8bc3f76a85ca5d8e702d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-26 13:26:53 +0900

    11-4. ユースケースの実装

---
+++ application/circle/CircleApplicationService.go
@@ -0,0 +1,142 @@
+package circle
+
+import (
+	"errors"
+	c "nrsDDD/domain/models/circle"
+	u "nrsDDD/domain/models/user"
+	cs "nrsDDD/domain/services/circle"
+)
+
+type CircleApplicationService struct {
+	circleFactory    c.CircleFactoryInterface
+	circleRepository c.CircleRepositoryInterface
+	circleService    cs.CircleService
+	userRepository   u.UserRepositoryInterface
+}
+
+func New(cf c.CircleFactoryInterface, cr c.CircleRepositoryInterface, cse cs.CircleService, ur u.UserRepositoryInterface) (*CircleApplicationService, error) {
+	return &CircleApplicationService{
+		circleFactory:    cf,
+		circleRepository: cr,
+		circleService:    cse,
+		userRepository:   ur,
+	}, nil
+}
+
+func (cas *CircleApplicationService) Create(command CircleCreateCommand) error {
+	ownerId, err := u.NewUserId(command.UserId)
+	if err != nil {
+		return err
+	}
+	owner, err := cas.userRepository.FindById(ownerId.Value)
+	if err != nil {
+		return err
+	}
+	if owner == nil {
+		return errors.New("ユーザーが見つかりません")
+	}
+	name, err := c.NewCircleName(command.Name)
+	if err != nil {
+		return err
+	}
+	circle, err := cas.circleFactory.Create(*name, *owner)
+	if err != nil {
+		return err
+	}
+	err = cas.circleRepository.Save(*circle)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cas *CircleApplicationService) Join(command CircleJoinCommand) error {
+	memberId, err := u.NewUserId(command.UserId)
+	if err != nil {
+		return err
+	}
+	member, err := cas.userRepository.FindById(memberId.Value)
+	if err != nil {
+		return err
+	}
+	if member == nil {
+		return errors.New("ユーザーが見つかりません")
+	}
+
+	id, err := c.NewCircleId(command.CircleId)
+	if err != nil {
+		return err
+	}
+	circle, err := cas.circleRepository.FindById(*id)
+	if err != nil {
+		return err
+	}
+	if circle == nil {
+		return errors.New("サークルが見つかりません")
+	}
+
+	if len(circle.Members) >= 29 {
+		return errors.New("メンバーがいっぱいです")
+	}
+
+	circle.Members = append(circle.Members, *member)
+
+	err = cas.circleRepository.Save(*circle)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cas CircleApplicationService) Invite(command CircleInviteCommand) error {
+	fromuserId, err := u.NewUserId(command.FromUserId)
+	if err != nil {
+		return err
+	}
+	fromUser, err := cas.userRepository.FindById(fromuserId.Value)
+	if err != nil {
+		return err
+	}
+	if fromUser == nil {
+		return errors.New("ユーザーが見つかりません")
+	}
+
+	invitedUserId, err := u.NewUserId(command.InvitedUserId)
+	if err != nil {
+		return err
+	}
+	invitedUser, err := cas.userRepository.FindById(invitedUserId.Value)
+	if err != nil {
+		return err
+	}
+	if invitedUser == nil {
+		return errors.New("ユーザーが見つかりません")
+	}
+
+	circleId, err := c.NewCircleId(command.CircleId)
+	if err != nil {
+		return err
+	}
+	circle, err := cas.circleRepository.FindById(*circleId)
+	if err != nil {
+		return err
+	}
+	if circle == nil {
+		return errors.New("サークルが見つかりません")
+	}
+
+	if len(circle.Members) >= 29 {
+		return errors.New("メンバーがいっぱいです")
+	}
+
+	// 未実装
+	// circleInvitation, err := NewCircleInvitation(circle, fromUser, invitedUser)
+	// if err != nil {
+	// 	return err
+	// }
+	// err = cas.circleInvitationRepository.Save(circleInvitation)
+	// if err != nil {
+	// 	return err
+	// }
+	return nil
+}
